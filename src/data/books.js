8e7e30f1e3387f8d14a7819acba562ff5322a518
SharmaLlama <sharmautkarsh0504@gmail.com>
2026-02-01 22:46:36 +1000

    books are own file

---
+++ src/data/books.js
@@ -0,0 +1,130 @@
+export const books = [
+    {
+      title: "The Three-Body Problem",
+      author: "Cixin Liu",
+      cover: "https://images-na.ssl-images-amazon.com/images/P/0765382032.01._SCLZZZZZZZ_.jpg",
+      rating: 3.5,
+      category: "fiction",
+      review: "First book of Lui Cixin's series. Great First intro about X. First book of Lui Cixin's series. Great First intro about XFirst book of Lui Cixin's series. Great First intro about XFirst book of Lui Cixin's series. Great First intro about XFirst book of Lui Cixin's series. Great First intro about XFirst book of Lui Cixin's series. Great First intro about XFirst book of Lui Cixin's series. Great First intro about XFirst book of Lui Cixin's series. Great First intro about X.First book of Lui Cixin's series. Great First intro about X.First book of Lui Cixin's series. Great First intro about X.First book of Lui Cixin's series. Great First intro about X ",
+      year: 2025
+    },
+    {
+      title: "The Dark Forest",
+      author: "Cixin Liu",
+      cover: "https://m.media-amazon.com/images/I/81yrzReGlRL._SL1500_.jpg",
+      rating: 4,
+      category: "fiction",
+      review: "",
+      year: 2025
+    },
+    {
+      title: "The Death's End",
+      author: "Cixin Liu",
+      cover: "https://m.media-amazon.com/images/I/91MfIt8mhaL._SL1500_.jpg",
+      rating: 4,
+      category: "fiction",
+      review: "",
+      year: 2025
+    },
+
+    {
+      title: "The Da Vinci Code",
+      author: "Dan Brown",
+      cover: "https://m.media-amazon.com/images/I/71y4X5150dL._SL1500_.jpg",
+      rating: 4,
+      category: "fiction",
+      review: "Second book in the Robert Langdon series. Great story about the Da Vinci Code and the secrets of the church.",
+      year: 2025
+    },
+
+    {
+      title: "The Lost Symbol",
+      author: "Dan Brown",
+      cover: "https://m.media-amazon.com/images/I/81BlXbpNmuL._SL1500_.jpg",
+      rating: 4,
+      category: "fiction",
+      review: "Third book in the Robert Langdon series. Great story about the Lost Symbol and the secrets of the church.",
+      year: 2025
+    },
+
+    {
+      title: "Origin",
+      author: "Dan Brown",
+      cover: "https://m.media-amazon.com/images/I/51uorhgbCpL.jpg",
+      rating: 4,
+      category: "fiction",
+      review: "Fourth book in the Robert Langdon series. Great story about the Origin and the secrets of the church.",
+      year: 2025
+    },
+
+    
+    {
+      title: "Can't Hurt Me: Master Your Mind and Defy the Odds",
+      author: "David Goggins",
+      cover: "https://m.media-amazon.com/images/I/81YJFNc54lL._SL1500_.jpg",
+      rating: 4,
+      category: "non-fiction",
+      review: "",
+      year: 2025
+    },
+    {
+      title: "Atomic Habits",
+      author: "James Clear",
+      cover: "https://images-na.ssl-images-amazon.com/images/P/0735211299.01._SCLZZZZZZZ_.jpg",
+      rating: 4.5,
+      category: "non-fiction",
+      review: "Changed my approach to building consistent learning habits. 1% better every day.",
+      year: 2025
+    },
+
+    {
+      title: "How to Win Friends and Influence People",
+      author: "Dale Carnegie",
+      cover: "https://m.media-amazon.com/images/I/71K7wW4VEoL._SL1500_.jpg",
+      rating: 5,
+      category: "non-fiction",
+      review: "Best book on how to win friends and influence people. It's a classic.",
+      year: 2025
+    },
+
+    {
+      title: "Never Split the Difference: Negotiating as if your life Depends on it",
+      author: "Chris Voss",
+      cover: "https://m.media-amazon.com/images/I/81PPq8CP4sL._SL1500_.jpg",
+      rating: 1,
+      category: "non-fiction",
+      review: "Best book on negotiation. It's a classic.",
+      year: 2025
+    },
+
+    {
+      title: "7 Habits of Highly Effective People",
+      author: "Stephen R. Covey",
+      cover: "https://m.media-amazon.com/images/I/71rmHeQeuRL._SL1255_.jpg",
+      rating: 2,
+      category: "non-fiction",
+      review: "Best book on the 7 habits of highly effective people. It's a classic.",
+      year: 2025
+    },
+
+    {
+      title: "Outlive",
+      author: "Dr. Peter Attia",
+      cover: "https://m.media-amazon.com/images/I/71mTMyT9Q0L._SL1500_.jpg",
+        rating: 3,
+      category: "non-fiction",
+      review: "Best book on how to live a long and healthy life. It's a classic.",
+      year: 2025
+    },
+
+    {
+      title: "Sapiens: A Brief History of Humankind",
+      author: "Yuval Noah Harari",
+      cover: "https://m.media-amazon.com/images/I/713jIoMO3UL._SL1500_.jpg",
+        rating: 5,
+      category: "non-fiction",
+      review: "Best book on the history of humanity. It's a classic.",
+      year: 2025
+    },
+  
+];
