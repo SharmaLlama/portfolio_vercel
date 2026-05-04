abf7da0d024fbbebfd6c83c2b4e564160481f4f1
SharmaLlama <sharmautkarsh0504@gmail.com>
2026-05-04 21:46:59 +1000

    new blog posts

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,10 +1,55 @@
 export const blogPosts = [
-  {
-    title: 'Understanding Raft Consensus: A Visual Guide (TEMPLATE)',
-    date: 'January 15, 2026',
-    readTime: '12 min read',
-    excerpt: 'A deep dive into how Raft achieves distributed consensus through leader election and log replication. Includes interactive visualizations and code examples.',
-    tags: ['Distributed Systems', 'Consensus', 'Go'],
-    link: '#'
+  {             
+    title: 'Tokenisation and Embeddings in Transformers',
+    date: 'May 4, 2026', 
+    readTime: '13 min read',
+    excerpt: 'A deep dive into the tokenisation and embeddings in transformers. This post is part of my series on Transformers from scratch.',
+    tags: ['Machine Learning', 'Transformers', 'Tokenisation', 'Embeddings'],                                                                                                                                                                       
+    link: 'https://blog.utkarshsharma.io/posts/tokenisation-embeddings/'
   },
+
+  // {
+  //   title: 'The mathematical approximation behind Model Flops Utilisation (MFU)',
+  //   date: 'April 29, 2026',
+  //   readTime: '12 min read',
+  //   excerpt: 'A deep dive into how the mathematical approximation behind Model Flops Utilisation (MFU) is calculated.',
+  //   tags: ['Machine Learning', 'Model Flops Utilisation', 'MFU'],
+  //   link: '#'
+  // },
+
+  // {
+  //   title: 'How Attention works in Transformers, deep dive into the mathematics and intuition',
+  //   date: 'April 29, 2026',
+  //   readTime: '12 min read',
+  //   excerpt: 'A deep dive into how Attention works in Transformers.',
+  //   tags: ['Machine Learning', 'Transformers', 'Attention'],
+  //   link: '#'
+  // },
+
+  // {
+  //   title: 'How to train a model with limited compute',
+  //   date: 'January 15, 2026',
+  //   readTime: '12 min read',
+  //   excerpt: 'A deep dive into how to train a model with limited compute.',
+  //   tags: ['Machine Learning', 'Model Training', 'Limited Compute'],
+  //   link: '#'
+  // },
+
+  // {
+  //   title: 'Does increasing grad accumulation step increase the VRAM usage or not?',
+  //   date: 'January 15, 2026',
+  //   readTime: '12 min read',
+  //   excerpt: 'A deep dive into how increasing the grad accumulation step affects the VRAM usage.',
+  //   tags: ['Machine Learning', 'Model Training', 'Grad Accumulation', 'VRAM Usage'],
+  //   link: '#'
+  // },
+
+  // {
+  //   title: 'The overall transformer architecture, loss function and training process',
+  //   date: 'January 15, 2026',
+  //   readTime: '12 min read',
+  //   excerpt: 'A deep dive into the overall transformer architecture, loss function and training process.',
+  //   tags: ['Machine Learning', 'Transformers', 'Architecture', 'Loss Function', 'Training Process'],
+  //   link: '#'
+  // },
 ];
